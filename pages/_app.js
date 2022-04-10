49c7dc50995222f0b678d93e6bee44c62e51317e
IDerr <ibrahim@derraz.fr>
2022-04-10 03:27:50 +0200

    ❤ Add chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
